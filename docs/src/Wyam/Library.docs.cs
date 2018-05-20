b1d30c284d326116c5d615c8042e534b8d71e45b
Stephen Price <stephen@lythixdesigns.com>
2018-05-20 09:43:52 +0800

    Added .git ignore. Added Tests project

---
+++ docs/src/Wyam/Library.docs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wyam
+{
+    /// <example>
+    ///   <code>
+    ///     SqlPackageExtract(settings =>
+    ///     {
+    ///         settings.Quiet = true;
+    ///     });
+    ///   </code>
+    /// </example>
+    /// <summary>
+    /// Library Class to rule them all
+    /// These examples could be in a seperate file so not to bloat the main code file.
+    /// Is there a way to decorate a method in the other file? Not sure, but somethign to investigate
+    /// Can you have a partial method? :)
+    /// </summary>
+    public partial class Library
+    {
+    }
+}
---
+++ src/Wyam.Tests/TestXmlDoc.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace Wyam.Tests
+{
+    public class TestXmlDoc
+    {
+        
+        /// <summary>
+        /// Modified example from 
+        /// https://blog.stephencleary.com/2018/05/roslyn-unit-testing.html
+        /// Changed it to only return the Xml from the code which is compiled
+        /// </summary>
+        [Fact]
+        public void Library_ParseXmlDoc_ContainsText()
+        {
+            var code = @"public class SampleClass {
+              /// <summary>Text to find.</summary>
+              public void SampleMethod() { } }";
+            // var (assembly, xmldoc) = Compile(code);
+
+            var xmldoc = Compile(code);
+
+            var doc = xmldoc.Descendants("member").FirstOrDefault(); // .FirstOrDefault(x => x.Attribute("name")?.Value); //  == member.MemberXmldocIdentifier()).Element(elementName);
+            string expectedValue = "<summary>Text to find.</summary>";
+            Assert.Equal(expectedValue, string.Join("", doc.Nodes().Select(x => x.ToString(SaveOptions.DisableFormatting))));
+
+            // var type = assembly.Modules.SelectMany(x => x.Types).Single(x => x.Name == "SampleClass");
+            // var method = type.Methods.Single(x => x.Name == "SampleMethod");
+
+            // Assert.Equal("M:SampleClass.SampleMethod", method.XmldocIdentifier());
+            // AssertXmldoc("Text to find.", xmldoc, method);
+        }
+
+        //public static void AssertXmldoc(XDocument xmldoc, string expectedValue, IMemberDefinition member, string elementName = "summary")
+        //{
+        //    var doc = xmldoc.Descendants("member").FirstOrDefault(x => x.Attribute("name")?.Value == member.MemberXmldocIdentifier()).Element(elementName);
+        //    Assert.Equal(expectedValue, string.Join("", doc.Nodes().Select(x => x.ToString(SaveOptions.DisableFormatting))));
+        //}
+
+        //public static (AssemblyDefinition Dll, XDocument Xml) Compile(string code)
+        public static XDocument Compile(string code)
+        {
+            // Parse the C# code...
+            CSharpParseOptions parseOptions = new CSharpParseOptions()
+                .WithKind(SourceCodeKind.Regular) // ...as representing a complete .cs file
+                .WithLanguageVersion(LanguageVersion.Latest); // ...enabling the latest language features
+
+            // Compile the C# code...
+            CSharpCompilationOptions compileOptions =
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary) // ...to a dll
+                    .WithOptimizationLevel(OptimizationLevel.Release) // ...in Release configuration
+                    .WithAllowUnsafe(enabled: true); // ...enabling unsafe code
+
+            // Invoke the compiler...
+            CSharpCompilation compilation =
+                CSharpCompilation.Create("TestInMemoryAssembly") // ..with some fake dll name
+                    .WithOptions(compileOptions)
+                    .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location)); // ...referencing the same mscorlib we're running on
+
+            // Parse and compile the C# code into a *.dll and *.xml file in-memory
+            var tree = CSharpSyntaxTree.ParseText(code, parseOptions);
+            var newCompilation = compilation.AddSyntaxTrees(tree);
+
+            var peStream = new MemoryStream();
+            var xmlStream = new MemoryStream();
+            var emitResult = newCompilation.Emit(peStream, xmlDocumentationStream: xmlStream);
+            if (!emitResult.Success)
+                throw new InvalidOperationException("Compilation failed: " + string.Join("\n", emitResult.Diagnostics));
+
+            // Parse the *.dll (with Cecil) and the *.xml (with XDocument) // Cecil is mono so I'm not using that here. Only want the Xml from the assembly anyway
+            peStream.Seek(0, SeekOrigin.Begin);
+            xmlStream.Seek(0, SeekOrigin.Begin);
+            return XDocument.Load(xmlStream);
+            //return (AssemblyDefinition.ReadAssembly(peStream), XDocument.Load(xmlStream));
+        }
+    }
+}
